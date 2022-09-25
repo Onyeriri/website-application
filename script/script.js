8aafab05385b23396675856ec4323afdab841869
Onyeriri Victor <viktorchinonso@gmail.com>
2022-09-25 07:43:36 +0100

    Added laptop view to website

--- script/script.js
+++ script/script.js
@@ -2,6 +2,9 @@
 const navMenu = document.querySelector(".nav");
 const navButton = document.querySelector(".nav-btn");
 const closeBtn = document.querySelector("#close");
+const listItems = document.querySelectorAll(".list-item");
+const navTwo = document.querySelector(".nav-two");
+const arrs = Array.prototype.slice.call(listItems);
 
 // function for opening the nav bar
 navButton.addEventListener("click", () => {
@@ -14,3 +17,5 @@ closeBtn.addEventListener("click", () => {
   navMenu.classList.add("close-nav");
   navMenu.classList.remove("nav-open");
 });
+
+// function that add active class to the nav
